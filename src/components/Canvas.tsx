f111b25456e8fb57ff14ff2d86032a6fe6fe1832
ardigc <agordo.95@gmail.com>
2023-05-05 21:04:54 +0200

    Cambios

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,14 +7,18 @@ export default function Canvas({ url }: { url: string }) {
     width: 0,
     height: 0,
   })
+
   const mouseDownHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
     console.log('Mouse down')
+
     setCoord({ x: ev.nativeEvent.offsetX, y: ev.nativeEvent.offsetY })
     setIsDown(true)
     document.addEventListener('mouseup', mouseUpHandler, { once: true })
   }
   const mouseUpHandler = (ev: MouseEvent) => {
     console.log('Mouse up')
+    setCoord({ x: 0, y: 0 })
+    setSize({ height: 0, width: 0 })
     setIsDown(false)
   }
   const mouseMoveHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
@@ -22,15 +26,16 @@ export default function Canvas({ url }: { url: string }) {
       setSize({ width: ev.nativeEvent.offsetX, height: ev.nativeEvent.offsetY })
     }
   }
-
+  // console.log("height:", size.height - coord.y,)
+  // console.log("width:", size.width - coord.x)
+  // console.log(size)
   return (
-    <div className="relative">
-      <img
-        draggable={false}
-        src={url}
-        onMouseMove={mouseMoveHandler}
-        onMouseDown={mouseDownHandler}
-      />
+    <div
+      className="relative"
+      onMouseMove={mouseMoveHandler}
+      onMouseDown={mouseDownHandler}
+    >
+      <img draggable={false} src={url} />
       <div
         className="bg-green-500 bg-opacity-50 absolute"
         style={{
