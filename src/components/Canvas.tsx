7e386dd256539d4b70deef49cd6f382044efbd65
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-05 18:04:05 +0000

    Set coordinates

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,8 +2,11 @@ import { MouseEventHandler, useState } from 'react'
 
 export default function Canvas({ url }: { url: string }) {
   const [isDown, setIsDown] = useState(false)
+  const [coord, setCoord] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
   const mouseDownHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
     console.log('Mouse down')
+    const coord = { x: ev.nativeEvent.offsetX, y: ev.nativeEvent.offsetY }
+    setCoord(coord)
     setIsDown(true)
     document.addEventListener('mouseup', mouseUpHandler, { once: true })
   }
@@ -13,20 +16,27 @@ export default function Canvas({ url }: { url: string }) {
   }
   const mouseMoveHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
     if (isDown) {
-      console.log(ev.nativeEvent.offsetX)
-      console.log(ev.nativeEvent.offsetY)
+      const width1 = ev.nativeEvent.offsetX
+      const heigth1 = ev.nativeEvent.offsetY
+      const size = { width: width1, heigth: heigth1 }
     }
   }
 
   return (
-    <div>
+    <div className="relative">
       <img
         draggable={false}
         src={url}
         onMouseMove={mouseMoveHandler}
         onMouseDown={mouseDownHandler}
       />
-      <div className="bg-green-500 bg-opacity-50" style={{}}></div>
+      <div
+        className="bg-green-500 bg-opacity-50 absolute"
+        style={{
+          top: coord.y,
+          left: coord.x,
+        }}
+      ></div>
     </div>
   )
 }
