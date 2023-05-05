55f2020870e24defdab2f5e1f4ab242824426b72
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-05 17:39:21 +0000

    Catch drawing action

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,3 +1,31 @@
+import { MouseEventHandler, useState } from 'react'
+
 export default function Canvas({ url }: { url: string }) {
-  return <img src={url} />
+  const [isDown, setIsDown] = useState(false)
+  const mouseDownHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
+    console.log('Mouse down')
+    setIsDown(true)
+  }
+  const mouseUpHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
+    console.log('Mouse up')
+    setIsDown(false)
+  }
+  const mouseMoveHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
+    if (isDown) {
+      console.log(ev.clientX)
+      console.log(ev.clientY)
+    }
+  }
+  return (
+    <div>
+      <img
+        draggable={false}
+        src={url}
+        onMouseMove={mouseMoveHandler}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+      />
+      <div className="bg-green-500 bg-opacity-50" style={{}}></div>
+    </div>
+  )
 }
