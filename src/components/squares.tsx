b19da7ca5c5cb90cd93705cd40581f05bbfeef5e
ardigc <agordo.95@gmail.com>
2023-05-06 18:21:55 +0200

    manita

--- src/components/squares.tsx
+++ src/components/squares.tsx
@@ -11,24 +11,61 @@ export default function Sqre({
   height: number
 }) {
   const [grab, setGrab] = useState(false)
+  let index: number
+  let cursor: string
+  if (grab) {
+    index = 20
+    cursor = 'grab'
+  } else {
+    index = 0
+    cursor = 'default'
+  }
+  console.log(grab)
   return (
-    <div onFocus={() => setGrab(true)} onBlur={() => setGrab(false)}>
+    <div onClick={() => setGrab(true)} onBlur={() => setGrab(false)}>
       <div
-        className="bg-green-500 bg-opacity-50 absolute"
+        className="bg-green-500 bg-opacity-50 absolute shadow-black shadow-xl"
+        id="div"
         tabIndex={0}
         style={{
           top: top,
           left: left,
           height: height,
           width: width,
+          zIndex: index,
+          cursor: cursor,
         }}
       ></div>
       {grab && (
         <div
-          onFocus={() => setGrab(true)}
+          onClick={() => setGrab(true)}
           onBlur={() => setGrab(false)}
-          className="h-2 w-2 bg-slate-300 absolute"
-          style={{ top: top + height / 2, left: left }}
+          className="h-3 w-3 bg-slate-300 absolute z-30 cursor-grab"
+          style={{ top: top + height / 2, left: left - 5 }}
+        ></div>
+      )}
+      {grab && (
+        <div
+          onClick={() => setGrab(true)}
+          onBlur={() => setGrab(false)}
+          className="h-3 w-3 bg-slate-300 absolute z-30 cursor-grab"
+          style={{ top: top + height / 2, left: left + width - 5 }}
+        ></div>
+      )}
+      {grab && (
+        <div
+          onClick={() => setGrab(true)}
+          onBlur={() => setGrab(false)}
+          className="h-3 w-3 bg-slate-300 absolute z-30 cursor-grab"
+          style={{ top: top + height - 5, left: left + width / 2 }}
+        ></div>
+      )}
+      {grab && (
+        <div
+          onClick={() => setGrab(true)}
+          onBlur={() => setGrab(false)}
+          className="h-3 w-3 bg-slate-300 absolute z-30 cursor-grab"
+          style={{ top: top - 5, left: left + width / 2 }}
         ></div>
       )}
     </div>
