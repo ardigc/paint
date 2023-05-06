0b2d1570158174e07809c5d43e2ea1c40728dd84
ardigc <agordo.95@gmail.com>
2023-05-06 18:50:14 +0200

    añadiendo codigo

--- src/components/squares.tsx
+++ src/components/squares.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, MouseEventHandler } from 'react'
 export default function Sqre({
   top,
   left,
@@ -10,20 +10,62 @@ export default function Sqre({
   width: number
   height: number
 }) {
+  const [rect, setRect] = useState<DOMRect>()
+  const [isDown, setIsDown] = useState(false)
+  const [size, setSize] = useState<{ width: number; height: number }>({
+    width: width,
+    height: height,
+  })
+
+  const [coord, setCoord] = useState<{ x: number; y: number }>({
+    x: rect?.x,
+    y: rect?.y,
+  })
+  console.log(coord)
+  const mouseDownHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
+    if (rect) {
+      const target = ev.target as HTMLElement
+      setRect(target.getBoundingClientRect())
+      console.log('Mouse down')
+      setCoord({ x: ev.clientX - rect.x, y: ev.clientY - rect.y })
+      setIsDown(true)
+      document.addEventListener('mouseup', mouseUpHandler, { once: true })
+    }
+  }
+  // console.log(rect?.x)
+  const mouseMoveHandler: MouseEventHandler<HTMLImageElement> = (ev) => {
+    if (isDown && rect) {
+      setSize({
+        width: ev.clientX - rect.x - coord.x,
+        height: ev.clientY - rect.y - coord.y,
+      })
+    }
+  }
+  const mouseUpHandler = () => {
+    console.log('Mouse up')
+
+    setIsDown(false)
+  }
+
   const [grab, setGrab] = useState(false)
   let index: number
   let cursor: string
   if (grab) {
+    top = size.height < 0 ? coord.y + size.height : coord.y
+    left = size.width < 0 ? coord.x + size.width : coord.x
+    width = Math.abs(size.width)
+    height = Math.abs(size.height)
     index = 20
-    cursor = 'grab'
+    cursor = 'move'
   } else {
     index = 0
     cursor = 'default'
   }
-  console.log(grab)
   return (
     <div onClick={() => setGrab(true)} onBlur={() => setGrab(false)}>
       <div
+        onMouseDown={mouseDownHandler}
+        onMouseMove={mouseMoveHandler}
         className="bg-green-500 bg-opacity-50 absolute shadow-black shadow-xl"
         id="div"
         tabIndex={0}
