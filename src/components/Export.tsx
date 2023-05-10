92a1e1b9e5333df8f12216f4b9fff76e5dbac9ca
ardigc <agordo.95@gmail.com>
2023-05-10 12:36:20 +0200

    mejorando export

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -32,6 +32,25 @@ export default function Export({
     const sqrArrCopy = sqrArr
     sqrArrCopy?.push(lastSqr)
     console.log(sqrArrCopy)
+    let csv: any
+    for (let count = 0; count < sqrArrCopy.length; count++) {
+      let keysAmount = Object.keys(sqrArrCopy[count]).length
+      let keysCounter = 0
+      if (count === 0) {
+        for (let key in sqrArrCopy[count]) {
+          csv += key + (keysCounter + 1 < keysAmount ? ',' : '\r\n')
+          keysCounter++
+        }
+      } else {
+        for (let key in sqrArrCopy[count]) {
+          csv +=
+            sqrArrCopy[count][key] +
+            (keysCounter + 1 < keysAmount ? ',' : '\r\n')
+          keysCounter++
+        }
+      }
+      keysCounter = 0
+    }
   }
 
   return (
