4b617dbc5b8d09d8bb4a4af1aaf4eb928616b915
ardigc <agordo.95@gmail.com>
2023-05-12 00:29:38 +0200

    Creado canvas img

--- src/components/PolygonCanvas.tsx
+++ src/components/PolygonCanvas.tsx
@@ -1,7 +1,21 @@
+import { useRef, useState } from 'react'
+
 export function PolygonCanvas({ url }: { url: string }) {
+  const canvasBeta = useRef<HTMLCanvasElement | null>(null)
+  const canvas = canvasBeta.current
+  const img = new Image()
+  img.src = url
+  const ctx = canvas?.getContext('2d')
+  img.onload = function () {
+    if (canvas && ctx) {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+    }
+  }
   return (
     <div>
-      <img src={url} />
+      <canvas ref={canvasBeta}></canvas>
     </div>
   )
 }
