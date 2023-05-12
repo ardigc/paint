901d909bcb5b74bb0ab1aaf27c810e4895cc16f9
ardigc <agordo.95@gmail.com>
2023-05-13 00:42:12 +0200

    realizado poligono

--- src/components/PolygonCanvas.tsx
+++ src/components/PolygonCanvas.tsx
@@ -11,6 +11,7 @@ export function PolygonCanvas({ url }: { url: string }) {
   const canvasBeta = useRef<HTMLCanvasElement | null>(null)
   const [coordOr, setCoordOr] = useState<CoordOr>(null)
   const [lines, setLines] = useState<Lines[]>([])
+  const [final, setFinal] = useState(false)
   const canvas = canvasBeta.current
   const rectCanvas = canvas?.getBoundingClientRect()
   const ctx = canvas?.getContext('2d')
@@ -40,12 +41,24 @@ export function PolygonCanvas({ url }: { url: string }) {
         { xOr: coordOr.xOr, yOr: coordOr.yOr, xFin: coordX, yFin: coordY },
       ])
     }
-    console.log(coordOr)
   }
-  function removeLines() {
+  const removeLines = () => {
     setLines([])
     setCoordOr(null)
-    ctx?.clearRect
+    setFinal(false)
+    const img = new Image()
+    img.src = url
+    img.onload = function () {
+      if (canvas && ctx) {
+        canvas.width = img.width
+        canvas.height = img.height
+        ctx.drawImage(img, 0, 0)
+      }
+    }
+  }
+  const closePoligon: MouseEventHandler<HTMLDivElement> = (ev) => {
+    ev.stopPropagation()
+    setFinal(true)
   }
   if (lines.length >= 1 && ctx) {
     ctx.beginPath()
@@ -53,7 +66,14 @@ export function PolygonCanvas({ url }: { url: string }) {
     lines.map((lines) => {
       ctx.lineTo(lines.xFin, lines.yFin)
     })
+    if (final) {
+      ctx?.closePath()
+    }
     ctx.stroke()
+    if (final) {
+      ctx.fillStyle = 'green'
+      ctx.fill()
+    }
   }
 
   return (
@@ -62,15 +82,26 @@ export function PolygonCanvas({ url }: { url: string }) {
         <canvas ref={canvasBeta}></canvas>
         {coordOr && (
           <div
-            className="absolute h-2 w-2 bg-slate-500 rounded-full"
+            className="absolute h-2 w-2 cursor-pointer border bg-orange-600 rounded-full"
+            onClick={closePoligon}
             style={{
-              top: coordOr.yOr,
-              left: coordOr.xOr,
+              top: coordOr.yOr - 5,
+              left: coordOr.xOr - 5,
             }}
           ></div>
         )}
+        {lines.length >= 1 &&
+          lines.map((lines) => (
+            <div
+              className="absolute h-2 w-2 border bg-orange-600 rounded-full"
+              style={{
+                top: lines.yFin - 5,
+                left: lines.xFin - 5,
+              }}
+            ></div>
+          ))}
       </div>
-      <button onClick={removeLines}>Limpiar linead</button>
+      <button onClick={removeLines}>Limpiar lineas</button>
     </div>
   )
 }
