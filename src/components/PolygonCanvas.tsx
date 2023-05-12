0a145c14a87460701c28ba92af2459b5b440ce31
ardigc <agordo.95@gmail.com>
2023-05-12 13:58:39 +0200

    pruebas con cavas

--- src/components/PolygonCanvas.tsx
+++ src/components/PolygonCanvas.tsx
@@ -2,29 +2,29 @@ import { MouseEventHandler, useRef, useState } from 'react'
 
 export function PolygonCanvas({ url }: { url: string }) {
   const canvasBeta = useRef<HTMLCanvasElement | null>(null)
-  const [rect, setRect] = useState<DOMRect>()
+  // const [rect, setRect] = useState<DOMRect>()
 
   const canvas = canvasBeta.current
+  const rectCanvas = canvas?.getBoundingClientRect()
   const img = new Image()
   img.src = url
   const ctx = canvas?.getContext('2d')
   img.onload = function () {
     if (canvas && ctx) {
-      const rect = img.getBoundingClientRect()
-      setRect(rect)
       canvas.width = img.width
       canvas.height = img.height
       ctx.drawImage(img, 0, 0)
     }
   }
   const clickHandler: MouseEventHandler<HTMLDivElement> = (ev) => {
-    if (!ctx) return
-
+    if (!ctx || !rectCanvas) return
+    const { left, top } = rectCanvas
     const { clientX, clientY } = ev
-    console.log(clientX, clientY)
+    const coordX = clientX - left
+    const coordY = clientY - top
     ctx.beginPath()
-    ctx.moveTo(0, 0) // Begin first sub-path
-    ctx.lineTo(200, 50)
+    ctx.moveTo(200, 50)
+    ctx.lineTo(coordX, coordY)
     ctx.stroke()
   }
   return (
