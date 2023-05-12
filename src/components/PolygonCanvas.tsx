e16c9655c9f0c89552d6cb876975d4567c4ae5d8
ardigc <agordo.95@gmail.com>
2023-05-12 15:42:16 +0200

    pruebas

--- src/components/PolygonCanvas.tsx
+++ src/components/PolygonCanvas.tsx
@@ -1,10 +1,18 @@
 import { MouseEventHandler, useRef, useState } from 'react'
-
+interface Lines {
+  xOr: number
+  yOr: number
+  xFin: number
+  yFin: number
+}
+type CoordOr = Pick<Lines, 'xOr' | 'yOr'> | null
+type CoordFin = Pick<Lines, 'xFin' | 'yFin'> | null
 export function PolygonCanvas({ url }: { url: string }) {
   const canvasBeta = useRef<HTMLCanvasElement | null>(null)
-  const [coordOr, setCoordOr] = useState({ x: -1, y: -1 })
+  const [coordOr, setCoordOr] = useState<CoordOr>(null)
+  const [coordFin, setCoordFin] = useState<CoordFin>(null)
   // const [rect, setRect] = useState<DOMRect>()
-
+  const [lines, setLines] = useState<Lines[]>([])
   const canvas = canvasBeta.current
   const rectCanvas = canvas?.getBoundingClientRect()
   const img = new Image()
@@ -18,23 +26,28 @@ export function PolygonCanvas({ url }: { url: string }) {
     }
   }
   const clickHandler: MouseEventHandler<HTMLDivElement> = (ev) => {
-    if (!ctx || !rectCanvas) return
+    if (!ctx || !rectCanvas || !coordOr) return
+
+    setLines((prev) => [
+      ...prev,
+      { xOr: coordOr?.xOr, yOr: coordOr.yOr, xFin: mouseX, yFin: mouseY },
+    ])
     const { left, top } = rectCanvas
     const { clientX, clientY } = ev
     const coordX = clientX - left
     const coordY = clientY - top
-    setCoordOr({ x: coordX, y: coordY })
+    setCoordOr({ xOr: coordX, yOr: coordY })
   }
   const mouseMoveHandler: MouseEventHandler<HTMLDivElement> = (ev) => {
     if (!ctx || !rectCanvas) return
-    if (coordOr.x !== -1 && coordOr.y !== -1) {
+    if (coordOr?.xOr && coordOr?.yOr) {
       ctx.clearRect(10, 10, 120, 100)
       ctx.beginPath()
-      ctx.moveTo(coordOr.x, coordOr.y)
+      ctx.moveTo(coordOr?.xOr, coordOr?.yOr)
       const { clientX, clientY } = ev
       const mouseX = clientX - rectCanvas.left
       const mouseY = clientY - rectCanvas.top
-
+      ctx.drawImage(img, 0, 0)
       ctx.lineTo(mouseX, mouseY)
       ctx.stroke()
     }
