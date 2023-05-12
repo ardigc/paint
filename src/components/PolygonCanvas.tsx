baaaa65d1329ea5c042362d4e143bf2df6ef9457
ardigc <agordo.95@gmail.com>
2023-05-12 14:43:33 +0200

    pruebas linea

--- src/components/PolygonCanvas.tsx
+++ src/components/PolygonCanvas.tsx
@@ -2,6 +2,7 @@ import { MouseEventHandler, useRef, useState } from 'react'
 
 export function PolygonCanvas({ url }: { url: string }) {
   const canvasBeta = useRef<HTMLCanvasElement | null>(null)
+  const [coordOr, setCoordOr] = useState({ x: -1, y: -1 })
   // const [rect, setRect] = useState<DOMRect>()
 
   const canvas = canvasBeta.current
@@ -22,14 +23,29 @@ export function PolygonCanvas({ url }: { url: string }) {
     const { clientX, clientY } = ev
     const coordX = clientX - left
     const coordY = clientY - top
-    ctx.beginPath()
-    ctx.moveTo(200, 50)
-    ctx.lineTo(coordX, coordY)
-    ctx.stroke()
+    setCoordOr({ x: coordX, y: coordY })
+  }
+  const mouseMoveHandler: MouseEventHandler<HTMLDivElement> = (ev) => {
+    if (!ctx || !rectCanvas) return
+    if (coordOr.x !== -1 && coordOr.y !== -1) {
+      ctx.clearRect(10, 10, 120, 100)
+      ctx.beginPath()
+      ctx.moveTo(coordOr.x, coordOr.y)
+      const { clientX, clientY } = ev
+      const mouseX = clientX - rectCanvas.left
+      const mouseY = clientY - rectCanvas.top
+
+      ctx.lineTo(mouseX, mouseY)
+      ctx.stroke()
+    }
   }
   return (
     <div>
-      <div className="relative w-max" onClick={clickHandler}>
+      <div
+        className="relative w-max"
+        onClick={clickHandler}
+        onMouseMove={mouseMoveHandler}
+      >
         <canvas ref={canvasBeta}></canvas>
       </div>
     </div>
