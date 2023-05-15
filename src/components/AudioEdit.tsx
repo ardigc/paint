6acc34f232e93cb3c6fe4069b5fd075dfb0785eb
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-15 16:53:47 +0000

    pruebas input

--- src/components/AudioEdit.tsx
+++ src/components/AudioEdit.tsx
@@ -1,8 +1,22 @@
-import { MouseEventHandler, useRef } from 'react'
+import { ChangeEventHandler, MouseEventHandler, useRef, useState } from 'react'
 
 export default function AudioEdit({ url }: { url: string }) {
   const audioRef = useRef<HTMLAudioElement>(null)
-  const duracion = audioRef.current?.duration / 60
+  const [time, setTime] = useState('')
+
+  const handleTimeChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value
+
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/
+    // if (regex.test(value)) {
+    //   const [minut, sec] = value.split(':');
+    //   const totalSec = parseInt(minut, 10) * 60 + parseInt(sec, 10);
+
+    //     if (totalSec >= 0 && totalSec <= 145) {
+    setTime(value)
+    // }
+    // }
+  }
   const playSegment = (startTime: number, endTime: number) => {
     if (audioRef.current) {
       audioRef.current.currentTime = startTime
@@ -11,6 +25,8 @@ export default function AudioEdit({ url }: { url: string }) {
       setTimeout(() => {
         audioRef.current?.pause()
       }, (endTime - startTime) * 1000)
+      const duration = audioRef.current?.duration
+      console.log(Math.trunc(duration / 60), duration % 60)
     }
   }
   const onClickHandle: MouseEventHandler<HTMLButtonElement> = () => {
@@ -21,6 +37,15 @@ export default function AudioEdit({ url }: { url: string }) {
       <audio ref={audioRef} src={url} controls>
         Your browser does not support the <code>audio</code> element.
       </audio>
+      <div>
+        <input
+          type="text"
+          placeholder="MM:SS"
+          value={time}
+          onChange={handleTimeChange}
+        />
+        <p>Ingresa un valor en el rango de 00:00 a 02:25.</p>
+      </div>
       <button onClick={onClickHandle}>
         reproducir segmento 10 sec a 15 sec
       </button>
