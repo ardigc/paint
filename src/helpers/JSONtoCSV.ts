f66cbb77703c127b457d9af23efd3e28fb4fb2f9
ardigc <agordo.95@gmail.com>
2023-05-15 13:19:17 +0200

    pruebas csv

--- src/helpers/JSONtoCSV.ts
+++ src/helpers/JSONtoCSV.ts
@@ -5,7 +5,17 @@ function jsonToCsv(data: any[]): string {
   const replacer = (key: string, value: any) => (value === null ? '' : value) // specify how you want to handle null values here
   const header = Object.keys(data[0])
   if (Array.isArray(data[0].lines)) {
-    return 'hola'
+    const header2 = Object.keys(data[0].lines[0])
+    const csv = [
+      header.join(';'), // header row first
+      header2.join(';'), // header row first
+      ...data.map((row) =>
+        header
+          .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+          .join(';')
+      ),
+    ]
+    return csv.join('\r\n')
   } else {
     const csv = [
       header.join(';'), // header row first
